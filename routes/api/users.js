171f53cf96a7dddac1d380b99bbf2e1ceefe4262
Ankit Verma <ankitverma3103@gmail.com>
2018-05-25 10:32:35 +0530

    user login api created

--- routes/api/users.js
+++ routes/api/users.js
@@ -57,4 +57,40 @@ router.post('/register', (req, res) => {
         })
 })
 
+// @route   POST api/users/login
+// @desc    Login user/ returning token
+// @access  public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email
+    User.findOne({
+            email
+        })
+        .then(user => {
+            // check for user
+            if (!user) {
+                return res.status(404).json({
+                    email: 'User not found'
+                });
+            }
+
+            // Check for password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({
+                            msg: 'Success!'
+                        })
+                    } else {
+                        return res.status(404).json({
+                            password: "Incorrect Password!"
+                        });
+                    }
+                })
+        })
+
+})
+
 module.exports = router;
