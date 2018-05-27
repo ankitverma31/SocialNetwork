be9166bc99d98391ac7e490c184effce722cda29
Ankit Verma <ankitverma3103@gmail.com>
2018-05-27 12:23:11 +0530

    Added experience route,validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,9 @@ const Profile = require('../../models/Profile');
 // Load Input Validation(Profile)
 const validateProfileInput = require('../../validation/profile');
 
+// Load Input Validation(Experience)
+const validateExperienceInput = require('../../validation/experience');
+
 //Load user model
 const User = require('../../models/Users');
 
@@ -204,5 +207,40 @@ router.get('/all', (req, res) => {
         }));
 });
 
+// @route   POST api/profile/experience
+// @desc    add experience to profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        const newExp = {
+            title: req.body.title,
+            company: req.body.company,
+            location: req.body.location,
+            from: req.body.from,
+            to: req.body.to,
+            current: req.body.current,
+            description: req.body.description
+        }
+
+        // Add experience to array
+        profile.experience.unshift(newExp);
+        profile.save().then(profile => res.json(profile));
+    })
+
+});
 
 module.exports = router;
