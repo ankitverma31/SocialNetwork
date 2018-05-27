e6a6df4f057d72374a0aa7538ac3a814c0bc51ee
Ankit Verma <ankitverma3103@gmail.com>
2018-05-27 17:15:32 +0530

    Added post validation,model and create,get post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load post model
+const Post = require('../../models/Post');
+
+// Load Input Validation(Post)
+const validatePostInput = require('../../validation/post');
+
 
 const router = express.Router();
 
@@ -11,3 +20,58 @@ router.get('/test', (req, res) => res.json({
 }));
 
 module.exports = router;
+
+// @route   POST api/posts
+// @desc    Create posts
+// @access  private
+router.post('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+
+});
+
+
+// @route   POST api/posts
+// @desc    Get Posts
+// @access  Public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({
+            date: -1
+        })
+        .then(posts => res.json(posts))
+        .catch(err => (res.status(404)).json({
+            nopostsfound: 'No posts found'
+        }));
+
+});
+
+// @route   POST api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => (res.status(404).json({
+            nopostfound: 'No post found with that ID'
+        })));
+
+});
