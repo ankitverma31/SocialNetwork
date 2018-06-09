845c39bd237c5e23a00a6da876f0c4903c7f5a25
Ankit Verma <ankitverma3103@gmail.com>
2018-06-09 20:29:25 +0530

    validating  gmailID

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const request = require("request");
+var rp = require("request-promise");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
@@ -35,39 +36,51 @@ router.post("/register", (req, res) => {
   if (!isValid) {
     return res.status(400).json(errors);
   }
-
-  User.findOne({
-    email: req.body.email
-  }).then(user => {
-    if (user) {
-      errors.email = "Email already exists!";
-      return res.status(400).json(errors);
-    } else {
-      var profilePicUrl =
-        "http://picasaweb.google.com/data/entry/api/user/" +
-        req.body.email +
-        "?alt=json&fields=gphoto:thumbnail";
-      request(profilePicUrl, function(error, response, body) {
-        const avatar = JSON.parse(body).entry.gphoto$thumbnail.$t;
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
+  var emailIdUrl =
+    "http://picasaweb.google.com/data/entry/api/user/" +
+    req.body.email +
+    "?alt=json&fields=gphoto:thumbnail";
+  rp(emailIdUrl)
+    .then(function() {
+      //if email id is valid gmail id
+      User.findOne({
+        email: req.body.email
+      }).then(user => {
+        if (user) {
+          errors.email = "Email already exists!";
+          return res.status(400).json(errors);
+        } else {
+          var profilePicUrl =
+            "http://picasaweb.google.com/data/entry/api/user/" +
+            req.body.email +
+            "?alt=json&fields=gphoto:thumbnail";
+          request(profilePicUrl, function(error, response, body) {
+            const avatar = JSON.parse(body).entry.gphoto$thumbnail.$t;
+            const newUser = new User({
+              name: req.body.name,
+              email: req.body.email,
+              avatar,
+              password: req.body.password
+            });
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                  .save()
+                  .then(user => res.json(user))
+                  .catch(err => console.log(err));
+              });
+            });
           });
-        });
+        }
       });
-    }
-  });
+    })
+    .catch(function(err) {
+      //if email id is not a valid gmail id
+      errors.email = req.body.email + " does not exist";
+      return res.status(404).json(errors);
+    });
 });
 
 // @route   POST api/users/login
