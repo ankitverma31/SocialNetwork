9b40e9d93d3ef070ec019b3b05bd7c4b917f205a
Ankit Verma <ankitverma3103@gmail.com>
2018-06-09 17:59:36 +0530

    stable production version(removed redux tool dependency)

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // options like actionSanitizer, stateSanitizer
+    })
+  : compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+  // other store enhancers if any
 );
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
